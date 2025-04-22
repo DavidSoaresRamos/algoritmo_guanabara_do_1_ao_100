d3cc6bcbdec107f19ef6adbc6a656130fd1279a7
Yuziio <clashdavid527@gmail.com>
2025-04-22 20:43:32 -0300

    mudanças

--- Ex4/src/somatorio.java
+++ Ex4/src/somatorio.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class somatorio {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o primeiro valor");
         int value1 = sc.nextInt();
