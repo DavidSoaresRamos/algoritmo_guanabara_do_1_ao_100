09f14aa12029daac9f6e1a7ba09d4ddb5b8f2eaf
Yuziio <clashdavid527@gmail.com>
2025-04-21 22:03:30 -0300

    Ex6

---
+++ Ex6/src/realNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class realNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um valor");
+        float realNumberValue = sc.nextFloat();
+        float doubleValue = realNumberValue * 2;
+        float thirdPart = realNumberValue / 3;
+
+        System.out.printf("O numero %f tem como seu dobro %f e sua terça parte %f", realNumberValue, doubleValue, thirdPart);
+    }
+}
