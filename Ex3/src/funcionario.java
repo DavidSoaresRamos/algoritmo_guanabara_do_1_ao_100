e261b946b43157aeacb42ae6a2d06224e04e0483
Yuziio <clashdavid527@gmail.com>
2025-04-21 21:21:16 -0300

    Ex2

---
+++ Ex3/src/funcionario.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class funcionario {
+    public static void main(String args[]){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Informe o nome do funcionario");
+    String name = sc.nextLine();
+    System.out.println("Informe o salario do funcionario");
+    int salary = sc.nextInt();
+    System.out.printf("O funcionario %s tem um salario de R$%d", name, salary );
+    }
+}
