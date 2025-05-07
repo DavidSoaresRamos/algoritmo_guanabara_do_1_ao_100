0f9f2bfb7d7aed354639474d7dd89f6ae8e208cc
Yuziio <clashdavid527@gmail.com>
2025-05-06 21:13:04 -0300

    Finalizando o codigo

--- Ex23/womanDay.java
+++ Ex23/womanDay.java
@@ -1,3 +1,7 @@
+// Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar descontos para todos, mas especialmente para mulheres. Faça um programa que leia nome, sexo e o valor das compras do cliente e calcule o preço com desconto. Sabendo que:
+// - Homens ganham 5% de desconto
+// - Mulheres ganham 13% de desconto
+
 import java.util.Scanner;
 
 public class womanDay {
@@ -5,10 +9,16 @@ public class womanDay {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe seu nome");
         String name = sc.nextLine();
-        System.out.println("Informe seu sexo");
-        char sexuality = sc.next();
+        System.out.println("Informe sua sexualidade");
+        String sexuality = sc.nextLine();
         System.out.println("Informe o valor da sua compra");
-        int value = sc.nextInt();
+        int purchaseValue = sc.nextInt();
+        System.out.printf("Nome: %s\n" , name);
+        if (sexuality.equals("Masculino")) {
+            System.out.printf("Valor da compra: %d", purchaseValue - (purchaseValue * 5 / 100));
+        } else if (sexuality.equals("Feminino")) {
+            System.out.printf("Valor da compra: %d", purchaseValue - (purchaseValue * 13 / 100));
+        }
 
     }
 }
