6e8da8c7b7850181e43cddb6da8acb8b2cb3ab8a
DavidRamos <clashdavid527@gmail.com>
2025-05-07 21:42:03 -0300

    Programa para converter um valor de reais para dolar

--- Ex9/wallet.java
+++ Ex9/wallet.java
@@ -1,5 +1,6 @@
 // Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
 
+
 import java.util.Scanner;
 public class wallet {
     public static void main(String args[]){
