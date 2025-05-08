f39f583a3e838797ae973d862a29bc1b5520a169
DavidRamos <clashdavid527@gmail.com>
2025-05-07 21:32:27 -0300

    Calculo do salario baseado nos dias trabalhados

--- Ex15/workingDays.java
+++ Ex15/workingDays.java
@@ -1,4 +1,5 @@
 // Crie um programa que leia o número de dias trabalhados em um mês e mostre o salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25 por hora trabalhada.
+
 import java.util.Scanner;
 
 public class workingDays {
