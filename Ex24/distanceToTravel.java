423835e0903d529a2c9832c42f8e3643626e9880
DavidRamos <clashdavid527@gmail.com>
2025-05-07 21:38:03 -0300

    Programa para verificar a kilometragem a ser percorrida e o valor da passagem

--- Ex24/distanceToTravel.java
+++ Ex24/distanceToTravel.java
@@ -2,6 +2,7 @@
 // percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para
 // viagens até 200Km e R$0.45 para viagens mais longas.
 
+
 import java.util.Scanner;
 
 public class distanceToTravel {
