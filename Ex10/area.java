8774812e4c02baabcffb826cee7c4a2358660ca9
DavidRamos <clashdavid527@gmail.com>
2025-05-07 21:30:05 -0300

    Calculo de area para pintura e tinta necessaria para o serviço

--- Ex10/area.java
+++ Ex10/area.java
@@ -1,5 +1,6 @@
 // Faça um algoritmo que leia a largura e altura de uma parede, calcule e mostre a área a ser pintada e a quantidade de tinta necessária para o serviço, sabendo que cada litro de tinta pinta uma área de 2metros quadrados.
 
+
 import java.util.Scanner;
 
 public class area {
