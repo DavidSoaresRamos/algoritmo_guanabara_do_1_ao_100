fa0dc8ab61eb9d496d3c8ada8bf339cec6a31758
DavidSoaresRamos <clashdavid527@gmail.com>
2025-09-11 08:09:08 -0300

    Calculo de IMC

--- Ex34/src/imcCalculator.java
+++ Ex34/src/imcCalculator.java
@@ -11,6 +11,18 @@ class imcCalculator {
 
         double imc = weight / (height * height);
 
-        System.out.printf("O seu IMC é de: %.2f", imc);
+        System.out.printf("O seu IMC é de: %.2f ", imc);
+
+        if (imc < 18.5) {
+            System.out.println("\n Abaixo do peso");
+        } else if ((imc >= 18.5) && (imc < 25)) {
+            System.out.println("\n Peso Ideal");
+        } else if ((imc >= 25) && ( imc < 30)) {
+            System.out.println("\n Sobrepeso");
+        } else if ((imc >= 30) && (imc < 40)) {
+            System.out.println("\n Obesidade");
+        } else if (imc > 40) {
+            System.out.println("\n Obesidade mórbida");
+        }
     }
 }
